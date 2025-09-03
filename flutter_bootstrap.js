49aa2e63014ef86ec8af14dd9e1d9009cf4af8d2
Valnor Cunha <manchadesigner@gmail.com>
2025-09-03 11:09:09 -0300

    Publicar somente build/web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2609253694"
+    serviceWorkerVersion: "3759125011"
   }
 });
